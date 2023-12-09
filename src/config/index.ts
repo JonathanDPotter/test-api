44849484c3c8504b3482ec886c5e02f3a4d56886
Jonathan Potter <jonathan.d.potter@outlook.com>
2023-12-09 11:47:16 -0500

    updated config

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ const [port, host, dbHost, user, password, database] = [
   process.env.HOST,
   process.env.DB_HOST,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   process.env.DB_SCHEMA,
 ];
 
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,8 @@
 import mysql from "mysql2";
 import config from "../config";
 
+console.log(config.mysql)
+
 const connection = mysql.createPool(config.mysql);
 
 export default connection;
