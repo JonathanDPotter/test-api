5a119c306b38fa18b9cd4da85f02eb7c8d74ae55
Jonathan Potter <jonathan.d.potter@outlook.com>
2023-12-09 11:29:40 -0500

    added one.ts

--- dist/middleware/errorHanlder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, _req, res) => {
-    console.error(err);
-    res.json({ name: err.name, msg: err.message });
-};
-exports.default = errorHandler;
-//# sourceMappingURL=errorHanlder.js.map
